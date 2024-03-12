e40791044f0082f7282336feb4ea9589006daf90
MidhunManu <midhunmanu384@gmail.com>
2024-03-12 21:21:15 +0530

    made stylish

--- foo.js
+++ foo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("button").click(function() {
 		let task = $("input").val().trim();
 		if(task !== "") {
-			$("ol").append(`<li>${task} <button class="done mx-5">done</button></li>`);
+			$("ol").append(`<li>${task} <button class="done btn btn-danger mx-5">done</button></li>`);
 			$(".done").click(function() {
 				$(this).parent().hide();
 			});
